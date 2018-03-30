71ad3bdff427e440aade8f6879371b5d9704d44f
Brian Cilenti <briancilenti@gmail.com>
2018-03-30 10:33:21 -0700

    housekeeping commit

--- api/routes/blogs.js
+++ api/routes/blogs.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const Blog = require('../models/blog');
+const mogoose = require('mongoose');
 
 // .get(url, handler ).  just "/" because any request that reaches this file
 // will already have a '/blogs' in the url.
@@ -12,17 +14,42 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  const newBlog = {
-    _id: Math.random().toFixed(8) * 1000,
+  const blog = new Blog({
+    _id: new mongoose.Types.ObjectId(),
     title: req.body.title,
     date: new Date(),
     body: req.body.body
-  }
-  res.status(200).json({
-    message: 'Handling POST requests to /blogs',
-    blog: newBlog
   });
+  blog
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Created blog sucessfully",
+        createdBlog: {
+          _id: result._id,
+          title: result.title,
+          date: result.date,
+          body: result.body,
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/blogs/" + result._id
+          }
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
+///// previousl placeholder GET response /////
+// res.status(200).json({
+//   message: 'Handling POST requests to /blogs',
+//   blog: newBlog
+// });
 
 //route for individual blog. Using :id variable to refrence specific blog
 router.get('/:blogId', (req, res, next) => {
